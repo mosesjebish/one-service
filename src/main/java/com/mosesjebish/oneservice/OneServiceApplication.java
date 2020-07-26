705af050bf3710fe1cfb159c208a4f778981da52
mosesjebish <mosesjebish@users.noreply.github.com>
2020-07-26 16:17:28 +0530

    adding swagger ui

--- src/main/java/com/mosesjebish/oneservice/OneServiceApplication.java
+++ src/main/java/com/mosesjebish/oneservice/OneServiceApplication.java
@@ -1,11 +1,13 @@
 package com.mosesjebish.oneservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAutoConfiguration
 public class OneServiceApplication {
 
 	public static void main(String[] args) {
